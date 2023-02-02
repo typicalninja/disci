299ee5efd041372070ff102a185b6c7aeb5feba5
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-02-02 14:58:11 +0530

    Remove accidental commit of .docusaurus

--- apps/docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('D:\\coding\\npm-pkgs\\disci\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('D:\\coding\\npm-pkgs\\disci\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('D:\\coding\\npm-pkgs\\disci\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('D:\\coding\\npm-pkgs\\disci\\apps\\docs\\src\\css\\custom.css'),
-];
