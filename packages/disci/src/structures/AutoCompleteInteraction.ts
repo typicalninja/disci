0cab93957dce4b2f0c8c782dae4c5b2f55f73e48
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-02-05 20:19:12 +0530

    feat: :sparkles: Add Autocomplete support

--- packages/disci/src/structures/AutoCompleteInteraction.ts
+++ packages/disci/src/structures/AutoCompleteInteraction.ts
@@ -1,19 +1,31 @@
+import {
+  APIApplicationCommandOptionChoice,
+  InteractionResponseType,
+} from "discord-api-types/v10";
 import { DisciTypeError } from "../utils/helpers";
 import type { IBase } from "./Base";
 import { BaseInteraction } from "./BaseInteraction";
 
 // TODO
 export class AutoCompleteInteraction extends BaseInteraction implements IBase {
-    /**
-     * Sends the autoComplete results.
-     */
-    sendResult(results: []) {
-        if(!Array.isArray(results)) throw new DisciTypeError()
-    }
-    /**
-     * Alias to doing sendResult with 0 results
-     */
-    noResults() {
-        return this.sendResult([]);
-    }
+  /**
+   * Sends the autoComplete choices.Set to empty array for "No choices"
+   */
+  sendChoices(choices: APIApplicationCommandOptionChoice[]) {
+    if (!Array.isArray(choices))
+      throw new DisciTypeError(`Choices must be a array`);
+    this._respond({
+      type: InteractionResponseType.ApplicationCommandAutocompleteResult,
+      data: {
+        choices,
+      },
+    });
+    return this;
+  }
+  /**
+   * Alias to doing sendChoices with empty ([]) choices
+   */
+  noResults() {
+    return this.sendChoices([]);
+  }
 }
