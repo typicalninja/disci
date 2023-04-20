2a1be48402ac374faff46f391d330cf3caf19f1b
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-04-20 22:39:36 +0530

    Cleanup

--- packages/disci/src/structures/ApplicationCommand.ts
+++ packages/disci/src/structures/ApplicationCommand.ts
@@ -2,6 +2,7 @@ import {
   APIApplicationCommandInteraction,
   APIChatInputApplicationCommandInteraction,
   APIUserApplicationCommandInteraction,
+  APIUserInteractionDataResolved,
   ApplicationCommandType,
   InteractionType,
   Snowflake,
@@ -10,7 +11,7 @@ import type { InteractionHandler } from '../InteractionHandler'
 import type { IBase } from './Base'
 import { BaseInteraction, InteractionOptions } from './BaseInteraction'
 import type { default as Message } from './primitives/Message'
-import type User from './primitives/User'
+import User from './primitives/User'
 import type Member from './primitives/Member'
 
 /**
@@ -48,12 +49,19 @@ export abstract class ApplicationCommand extends BaseInteraction implements IBas
     this.commandType = data.type
     this.commandName = data.name
     this.commandId = data.id
-
     this.resolved = {
       users: new Map(),
       members: new Map(),
       roles: new Map(),
       messages: new Map(),
+    }
+
+    if (this.commandType !== ApplicationCommandType.Message) {
+      // does not exist in MessageContextMenu
+      const userResolved = rawData.data?.resolved as APIUserInteractionDataResolved
+      for (const [id, user] of Object.entries(userResolved?.users)) {
+        this.resolved.users.set(id, new User(this.handler, user))
+      }
     }
   }
   /**
@@ -101,13 +109,18 @@ export class MessageCommandInteraction extends ApplicationCommand {
 export class UserCommandInteraction extends ApplicationCommand implements ContextMenuInteraction {
   override commandType = ApplicationCommandType.User
   /**
-   * Id of the user
+   * Id of the Target user
    */
   targetId: Snowflake
+  /**
+   * Target User
+   */
+  targetUser?: User
   constructor(handler: InteractionHandler, rawData: APIUserApplicationCommandInteraction) {
     super(handler, rawData)
     const data = rawData.data
     this.targetId = data.target_id
+    this.targetUser = this.resolved.users.get(this.targetId)
   }
 }
 
