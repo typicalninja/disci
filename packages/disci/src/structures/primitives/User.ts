5e85f359dde4b358ff80c99993374c3c05fa6911
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-04-22 15:50:22 +0530

    feat(disci): :art: Implement partialUser

--- packages/disci/src/structures/primitives/User.ts
+++ packages/disci/src/structures/primitives/User.ts
@@ -1,16 +1,41 @@
-import type { APIUser, Snowflake } from 'discord-api-types/v10'
-import type { InteractionHandler } from '../../InteractionHandler'
-import type { IBase } from '../Base'
+import { type APIUser, type Snowflake, Routes } from 'discord-api-types/v10';
+import type { InteractionHandler } from '../../InteractionHandler';
+import type { IBase } from '../Base';
+
+/**
+ * Partial Class for accessing Discord Api with minimal data
+ */
+export class PartialUser implements IBase {
+  /**
+   * The handler than initiated this class
+   */
+  handler!: InteractionHandler
+  /**
+   * The user's id
+   */
+  id: Snowflake
+  constructor(handler: InteractionHandler, data: { id: string }) {
+    // assign the handler
+    Object.defineProperty(this, 'handler', { value: handler })
+    this.id = data.id;
+  }
+  /**
+   * Fetch the user this partial belongs to
+   */
+  async fetch(): Promise<User> {
+    const user = await this.handler.api.get<APIUser>(Routes.user(this.id))
+    return new User(this.handler, user)
+  }
+  toString() {
+    return `<@${this.id}>`
+  }
+}
 
 /**
  * Represents a Discord user
  * https://discord.com/developers/docs/resources/user#user-object
  */
-export default class User implements IBase {
-  /**
-   * The user's id
-   */
-  id: Snowflake
+export default class User extends PartialUser {
   /**
    * The username of this user.Not unique
    */
@@ -20,18 +45,11 @@ export default class User implements IBase {
    */
   discriminator: string
   /**
-   * The handler than initiated this class
-   */
-  handler!: InteractionHandler
-  /**
    * Create a new user from discord data
    * @param apiData - data from discord api
    */
   constructor(handler: InteractionHandler, public apiData: APIUser) {
-    // assign the handler
-    Object.defineProperty(this, 'handler', { value: handler })
-
-    this.id = apiData.id
+    super(handler, { id: apiData.id })
     this.discriminator = apiData.discriminator
     this.username = apiData.username
   }
@@ -40,9 +58,5 @@ export default class User implements IBase {
    */
   get tag() {
     return `${this.username}#${this.discriminator}`
-  }
-
-  toString() {
-    return `<@${this.id}>`
   }
 }
