c6ab893ffae60e6ac73c3a5be9c2f793cd265628
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-07-27 18:41:51 +0530

    update the cf worker example

--- apps/example_cf_worker/src/index.ts
+++ apps/example_cf_worker/src/index.ts
@@ -1,80 +1,142 @@
-import { InteractionHandler, NativeVerificationStrategy } from 'disci'
-import { IRequest } from 'itty-router'
-import { ThrowableRouter, withContent, StatusError, status } from 'itty-router-extras'
+import { InteractionHandler } from "disci";
+import { IRequest, Router, withContent, json, error } from "itty-router";
+import { verify } from "discord-verify";
+import { ButtonStyle, ComponentType } from "discord-api-types/v10";
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	EmbedBuilder,
+} from "@discordjs/builders";
 
 type RequestWith = {
-  content: string[]
-  headers: any
-} & IRequest
+	content: Record<string, unknown>;
+} & IRequest;
+
+const router = Router();
+const handler = new InteractionHandler();
+// public key will be obtained from the env
+let publicKey = "";
 
 export interface Env {
-  TOKEN: string
-  PUBLIC_KEY: string
-  // These objects are created before first use, then stashed here
-  // for future use
-  router?: ThrowableRouter
-  handler?: InteractionHandler
+	TOKEN: string;
+	PUBLIC_KEY: string;
 }
+
+handler.on("interactionCreate", async (interaction) => {
+		if (interaction.isCommand()) {
+			if (interaction.isChatInputInteraction()) {
+				const option = interaction.options.getString("auto");
+
+				const embed = new EmbedBuilder()
+					.setDescription("Test from cloudflare")
+					.setFooter({ text: `Test for ${option}` })
+					.setAuthor({ name: "Text" })
+					.setTimestamp(Date.now())
+					.setTitle("title boii")
+					.setThumbnail(
+						"https://cdn.discordapp.com/avatars/823448814186397696/014a5dc1e4b084e8958ace890690a821.png?size=4096",
+					)
+					.setImage(
+						"https://cdn.discordapp.com/avatars/823448814186397696/014a5dc1e4b084e8958ace890690a821.png?size",
+					);
+
+				const actionRow1 = new ActionRowBuilder<ButtonBuilder>();
+
+				const button1 = new ButtonBuilder()
+					.setCustomId("button1")
+					.setDisabled(true)
+          .setStyle(ButtonStyle.Primary)
+					.setLabel(`Button 1`);
+
+
+				const button2 = new ButtonBuilder()
+					.setCustomId("button2")
+					.setStyle(ButtonStyle.Primary)
+					.setLabel(`Button 2`);
+
+		  		actionRow1.addComponents(button1, button2);
+				console.log(JSON.stringify(actionRow1.toJSON()));
+				const msg = await interaction.respond({
+					content: `Hi from cloudflare`,
+					embeds: [embed.toJSON()],
+					components: [actionRow1.toJSON()],
+					fetchReply: true,
+				});
+
+				console.log(msg.embeds);
+			}
+		} else if (interaction.isAutoComplete()) {
+			const focused = interaction.options.getFocused(true);
+			console.log(focused);
+			interaction.respondWithChoices([
+				`Typical ${focused.value ?? "ninja"}`,
+				"No typical",
+			]);
+		} else if (interaction.isComponent()) {
+			console.log(`Component interaction`);
+      interaction.respond({
+        content: 'hey'
+      })
+		}
+});
+
+/* Router Handling */
+
+router.get("/", async () => {
+	if (publicKey === "") return new Response(`Public key is invalid 👎`);
+	return new Response(`Worker is working 🚀`);
+});
+
+router.post<RequestWith>(
+	"/interactions",
+	withContent,
+	async ({ content, headers }) => {
+		const signature = headers.get("x-signature-ed25519");
+		const timestamp = headers.get("x-signature-timestamp");
+		const rawBody = JSON.stringify(content);
+
+		const isValid = await verify(
+			rawBody,
+			signature,
+			timestamp,
+			publicKey,
+			crypto.subtle,
+		);
+		console.log("isValid", isValid);
+		if (!isValid) {
+			return new Response("Invalid Headers", { status: 401 });
+		}
+
+		const timeOutAbort = new AbortController();
+
+		const timeout = setTimeout(() => {
+			timeOutAbort.abort("Time out");
+		}, 3000);
+
+		try {
+			const response = await handler.processRequest(
+				content,
+				timeOutAbort.signal,
+			);
+			clearTimeout(timeout);
+			return json(response);
+		} catch (err) {
+			console.log("error", err);
+			return error(500, "Internal Server error");
+		}
+	},
+);
+
+router.all("*", () => new Response("Not Found 😕", { status: 404 }));
 
 export default {
-  async fetch(request: Request, env: Env): Promise<Response> {
-	// only 1 time
-    if (env.handler === undefined) {
-      env.handler = new InteractionHandler({
-       // add in dev: debug: (msg: string) => console.log(msg),
-        verificationStrategy: new NativeVerificationStrategy(env.PUBLIC_KEY),
-        rest: {
-          token: env.TOKEN,
-        },
-      })
-    }
+	fetch: async (req: Request, env: Env, ctx: ExecutionContext) => {
+		if (publicKey === "") {
+			publicKey = env.PUBLIC_KEY;
+			// most likely the token was also not set
+			handler.api.setToken(env.TOKEN);
+		}
 
-    if (env.router === undefined) {
-      env.router = buildRouter(env)
-    }
-
-    return env.router.handle(request)
-  },
-}
-
-function buildRouter(env: Env): ThrowableRouter {
-  const router = ThrowableRouter()
-
-  if (env.handler) {
-    env.handler.on('interactionCreate', (interaction) => {
-      if (interaction.isCommand()) {
-        if (interaction.isChatInputInteraction()) {
-          interaction.respond({
-            content: `Hi from cloudflare`,
-          })
-        }
-      }
-    })
-  }
-
-  router.get('/', async () => {
-    if (!env.handler) return new Response(`No: Handler Offline`)
-    return new Response(`Active and ready`)
-  })
-
-  router.post('/interactions', withContent, async ({ content, headers }: RequestWith) => {
-    //console.log(res)
-    if (!env.handler) throw new StatusError(500, 'Server Error')
-
-    const response = await env.handler
-      .handleRequest({
-        headers: {
-          'x-signature-ed25519': headers.get('x-signature-ed25519'),
-          'x-signature-timestamp': headers.get('x-signature-timestamp'),
-        },
-        body: content,
-      })
-      .catch((err) => console.log(err))
-    if (!response) throw new StatusError(500, 'server Error')
-    return status(response.status, response.content)
-  })
-
-  router.all('*', () => new Response('Not Found.', { status: 404 }))
-
-  return router
-}
+		return router.handle(req, env, ctx).then(json).catch(error);
+	},
+};
