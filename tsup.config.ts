c151ce1a57cf7a4f28fcfca2773afc2b1cc882ec
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-08-30 17:22:56 +0530

    refactor: update tsup

---
+++ tsup.config.ts
@@ -0,0 +1,40 @@
+
+import { defineConfig } from 'tsup';
+
+export function createTsupConfig({
+	entry = ['src/index.ts'],
+	external = [],
+	noExternal = [],
+	platform = 'node',
+	format = ['cjs', 'esm'],
+	target = 'es2022',
+	skipNodeModulesBundle = true,
+	clean = false,
+	shims = false,
+	minify = true,
+	splitting = false,
+	keepNames = true,
+	dts = true,
+	sourcemap = true,
+	esbuildPlugins = [],
+	outDir = 'dist',
+} = {}) {
+	return defineConfig({
+		entry,
+		external,
+		noExternal,
+		platform,
+		format,
+		skipNodeModulesBundle,
+		target,
+		clean,
+		shims,
+		minify,
+		splitting,
+		keepNames,
+		dts,
+		sourcemap,
+		esbuildPlugins,
+		outDir,
+	});
+}
