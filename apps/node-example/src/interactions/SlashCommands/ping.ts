7b8bc0dacbab766fcdba88c963ffef53f82d0581
typicalninja493 <65993466+typicalninja493@users.noreply.github.com>
2023-09-06 23:19:15 +0530

    refactor: cleanup, minor version of 0.0.5

--- apps/node-example/src/interactions/SlashCommands/ping.ts
+++ apps/node-example/src/interactions/SlashCommands/ping.ts
@@ -4,19 +4,8 @@ export default {
     name: 'ping',
     description: 'Check if the bot is alive',
     async run(interaction) {
-        
-       const msg = await interaction.deferReply({ fetchReply: true })
-
-        console.log(await msg.startThread({
-            name: 'Found request data',
-            autoArchiveDuration: ThreadAutoArchiveDuration.OneDay
-        }))
-        interaction.editReply({
-            files: [{
-                contentType: 'text/plain',
-                data: 'Test',
-                name: 'text.txt'
-            }]
+        interaction.reply({
+            content: 'Pong!'
         })
     },
 } satisfies SlashCommand
