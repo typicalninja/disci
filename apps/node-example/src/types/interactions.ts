19f669d8ed0b514bed20891f8fea65dadc47c610
typicalninja <65993466+typicalninja@users.noreply.github.com>
2024-04-20 14:33:20 +0530

    refactor: simplify examples

--- apps/node-example/src/types/interactions.ts
+++
@@ -1,8 +0,0 @@
-import type { AutoCompleteInteraction, ChatInputInteraction } from "disci";
-
-export interface SlashCommand {
-    name: string;
-    description: string;
-    run: (interaction: ChatInputInteraction) => unknown;
-    autoComplete?: (interaction: AutoCompleteInteraction) => unknown
-}
