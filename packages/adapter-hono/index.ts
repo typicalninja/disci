cb9b6aade8537f920dc21a08b699a35dcd9f0880
typicalninja <65993466+typicalninja@users.noreply.github.com>
2024-10-24 09:11:55 +0530

    feat: add toGenericRequest function to convert hono context

--- packages/adapter-hono/index.ts
+++ packages/adapter-hono/index.ts
@@ -1,5 +1,38 @@
-import { DiscordVerifyHeaders, type InteractionHandler } from "disci";
+import {
+	DiscordVerifyHeaders,
+	type GenericRequest,
+	type InteractionHandler,
+} from "disci";
+import type { Context } from "hono";
 import { createFactory } from "hono/factory";
+
+/**
+ * Converts a Hono context to a generic request object
+ * @example
+ * ```ts
+ * const handler = new InteractionHandler(...);
+ * const app = new Hono();
+ *
+ * app.post("/interactions", async (c) => handler.handleRequest(await toGenericRequest(c)));
+ * ```
+ * @param c - The Hono context
+ * @returns
+ */
+export async function toGenericRequest(c: Context): Promise<GenericRequest> {
+	const signature = c.req.header(DiscordVerifyHeaders.signature) ?? "";
+
+	const timestamp = c.req.header(DiscordVerifyHeaders.timestamp) ?? "";
+
+	return {
+		body: await c.req.text(),
+
+		headers: {
+			[DiscordVerifyHeaders.signature]: signature,
+
+			[DiscordVerifyHeaders.timestamp]: timestamp,
+		},
+	};
+}
 
 export function createRequestHandler(handler: InteractionHandler) {
 	const factory = createFactory();
@@ -39,6 +72,5 @@ export function createRequestHandler(handler: InteractionHandler) {
 			c.status(500);
 			return c.json({ e: "Internal server error occurred" });
 		}
-	})[// the user will be able to do: createRequestHandler(handler) instead of ...createRequestHandler(handler); // return the first handler
-	0];
+	})[0]; // the user will be able to do: createRequestHandler(handler) instead of ...createRequestHandler(handler); // return the first handler
 }
